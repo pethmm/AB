6a7fe8b307655569c246d32e5a1767943a8a0049
Pety <38808074+pethmm@users.noreply.github.com>
2025-10-28 21:10:44 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/abidecoder/main.go
@@ -0,0 +1,23 @@
+// cmd/abidecoder/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "abidecoder/internal/abidecoder"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := abidecoder.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
